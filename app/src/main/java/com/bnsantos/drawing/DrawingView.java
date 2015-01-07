ead93b5ff00fb4242a3857df474a801f54a8c461
Bruno Santos <brunonascimentosantos@gmail.com>
2015-01-07 15:47:48 -0200

    layout

---
+++ app/src/main/java/com/bnsantos/drawing/DrawingView.java
@@ -0,0 +1,19 @@
+package com.bnsantos.drawing;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by bruno on 07/01/15.
+ */
+public class DrawingView extends View {
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+        //TODO
+    }
+}
