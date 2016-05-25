a02e0e3505c562b3fe3ff31eed257eaf18f4d2f4
Bruno Santos <brunonascimentosantos@gmail.com>
2016-05-25 11:27:24 -0300

    gradle update new initial activity example using bottom sheet layout

---
+++ app/src/main/java/com/bnsantos/drawing/DrawingActivity.java
@@ -0,0 +1,95 @@
+package com.bnsantos.drawing;
+
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.UUID;
+
+public class DrawingActivity extends AppCompatActivity {
+  private DrawingView mView;
+  private ImageButton mCurrentColor;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_drawing);
+
+    ActionBar actionBar = getSupportActionBar();
+    actionBar.hide();
+
+    mView = (DrawingView) findViewById(R.id.drawing);
+    mCurrentColor = (ImageButton) findViewById(R.id.paint_black);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  public void setDrawerBackground(View v){
+    //TODO option to select any picture from gallery, also use picasso to avoid out of memmory
+    mView.setBackgroundResource(R.drawable.monalisa);
+  }
+
+  public void paintClicked(View v){
+    if(!v.getTag().equals(mCurrentColor.getTag())){
+      mCurrentColor.setImageResource(R.drawable.paint);
+      mCurrentColor = (ImageButton) v;
+      mCurrentColor.setImageResource(R.drawable.paint_pressed);
+      mView.setColor(mCurrentColor.getTag().toString());
+    }
+  }
+
+  public void erase(View v){
+    mView.setErase(true);
+  }
+
+  public void draw(View v){
+    mView.setErase(false);
+  }
+
+  public void newDrawing(View v){
+    //TODO confirmation dialog
+    mView.newDrawing();
+  }
+
+  public void saveImage(View v){
+    mView.setDrawingCacheEnabled(true);
+    String imgSaved = MediaStore.Images.Media.insertImage(
+        getContentResolver(), mView.getDrawingCache(),
+        UUID.randomUUID().toString()+".png", "drawing");
+
+    if(imgSaved!=null){
+      Toast.makeText(getApplicationContext(), "Drawing saved to Gallery!", Toast.LENGTH_SHORT).show();
+    }
+    else{
+      Toast.makeText(getApplicationContext(), "Oops! Image could not be saved.", Toast.LENGTH_SHORT).show();
+    }
+    mView.destroyDrawingCache();
+    mView.newDrawing();
+  }
+}
--- app/src/main/java/com/bnsantos/drawing/MainActivity.java
+++ app/src/main/java/com/bnsantos/drawing/MainActivity.java
@@ -1,97 +1,67 @@
 package com.bnsantos.drawing;
 
-import android.provider.MediaStore;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
-import java.util.UUID;
+import com.bnsantos.drawing.databinding.ActivityMainBinding;
 
 
-public class MainActivity extends ActionBarActivity {
-    private DrawingView mView;
-    private ImageButton mCurrentColor;
+public class             MainActivity extends AppCompatActivity {
+  private ActivityMainBinding binding;
+  private BottomSheetBehavior mBottomSheetBehavior;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    mBottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet);
 
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+    binding.start.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+      }
+    });
+    
+    binding.view.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Toast.makeText(MainActivity.this, "TODO", Toast.LENGTH_SHORT).show();
+      }
+    });
 
-        mView = (DrawingView) findViewById(R.id.drawing);
-        mCurrentColor = (ImageButton) findViewById(R.id.paint_black);
+    binding.bottomSheet.findViewById(R.id.phoneGallery).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Toast.makeText(MainActivity.this, "TODO phone gallery", Toast.LENGTH_SHORT).show();
+      }
+    });
+
+    binding.bottomSheet.findViewById(R.id.camera).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Toast.makeText(MainActivity.this, "TODO phone camera", Toast.LENGTH_SHORT).show();
+      }
+    });
+
+    binding.contentLayout.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+      }
+    });
+  }
+
+  @Override
+  public void onBackPressed() {
+    if(mBottomSheetBehavior.getState()==BottomSheetBehavior.STATE_EXPANDED){
+      mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+    }else{
+      super.onBackPressed();
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void setDrawerBackground(View v){
-        //TODO option to select any picture from gallery, also use picasso to avoid out of memmory
-        mView.setBackgroundResource(R.drawable.monalisa);
-    }
-
-    public void paintClicked(View v){
-        if(!v.getTag().equals(mCurrentColor.getTag())){
-            mCurrentColor.setImageResource(R.drawable.paint);
-            mCurrentColor = (ImageButton) v;
-            mCurrentColor.setImageResource(R.drawable.paint_pressed);
-            mView.setColor(mCurrentColor.getTag().toString());
-        }
-    }
-
-    public void erase(View v){
-        mView.setErase(true);
-    }
-
-    public void draw(View v){
-        mView.setErase(false);
-    }
-
-    public void newDrawing(View v){
-        //TODO confirmation dialog
-        mView.newDrawing();
-    }
-
-    public void saveImage(View v){
-        mView.setDrawingCacheEnabled(true);
-        String imgSaved = MediaStore.Images.Media.insertImage(
-                getContentResolver(), mView.getDrawingCache(),
-                UUID.randomUUID().toString()+".png", "drawing");
-
-        if(imgSaved!=null){
-            Toast.makeText(getApplicationContext(), "Drawing saved to Gallery!", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            Toast.makeText(getApplicationContext(), "Oops! Image could not be saved.", Toast.LENGTH_SHORT).show();
-        }
-        mView.destroyDrawingCache();
-        mView.newDrawing();
-    }
+  }
 }
