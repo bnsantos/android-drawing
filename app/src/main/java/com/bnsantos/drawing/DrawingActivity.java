01d7a1e6891d687b1083ac587d224f430719e42b
Bruno Santos <brunonascimentosantos@gmail.com>
2016-05-25 19:36:01 -0300

    adding file to media store

--- app/src/main/java/com/bnsantos/drawing/DrawingActivity.java
+++ app/src/main/java/com/bnsantos/drawing/DrawingActivity.java
@@ -1,5 +1,6 @@
 package com.bnsantos.drawing;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
@@ -9,6 +10,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
@@ -223,14 +225,22 @@ public class DrawingActivity extends AppCompatActivity implements View.OnClickLi
       Toast.makeText(DrawingActivity.this, "Error 2", Toast.LENGTH_SHORT).show();
     }
 
+
+    ContentValues values = new ContentValues();
+
+    values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+    values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+    values.put(MediaStore.MediaColumns.DATA, file.getPath());
+
+    getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
     finish();
 
     Intent shareIntent = new Intent();
-    shareIntent.setAction(Intent.ACTION_SEND);
-    shareIntent.putExtra(Intent.EXTRA_STREAM,Uri.fromFile(file));
-    shareIntent.setType("image/png");
+    shareIntent.setAction(Intent.ACTION_VIEW);
+    shareIntent.setDataAndType(Uri.fromFile(file), "image/jpg");
     shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-    startActivity(Intent.createChooser(shareIntent, "Share image"));
+    startActivity(Intent.createChooser(shareIntent, "View image"));
   }
 
   private File createDrawingFolder() {
